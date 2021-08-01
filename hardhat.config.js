3eae4b178b268cbc1ca982f4527c342d6e920f4b
JamesVitaly <jamesvitaly1993@gmail.com>
2021-08-01 17:33:37 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,11 +16,11 @@ module.exports = {
    }
   },
   etherscan: {
-    apiKey: '3U3G4BKNXH6P4AVVCGN22BC46IY5PZ4XSP' //TODO: change Etherscan API key
+    apiKey: '' //TODO: change Etherscan API key
   },
   networks: {
     ropsten: {
-      url: `https://ropsten.infura.io/v3/59d4e21f46a64e5c9d4c50750ca80c84`, //TODO: change Infura project Key
+      url: ``, //TODO: change Infura project Key
       accounts: {mnemonic: mnemonic}
     },
   }
