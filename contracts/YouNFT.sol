b43e61666fe0319046aa09b90060f99184643374
yellowsubcreative <me@andrewminton.co.uk>
2021-10-21 22:32:00 +0100

    Made ipfs to mint private funciton

--- contracts/YouNFT.sol
+++ contracts/YouNFT.sol
@@ -167,7 +167,7 @@ contract YourNFT is ERC721PresetMinterPauserAutoId, Ownable {
         return seed.mod(to - from) + from;
     }
 
-    function getIpfsIdToMint() public view returns (uint256 _nextIpfsId) {
+    function getIpfsIdToMint() private returns (uint256 _nextIpfsId) {
         require(
             !isAllTokenMinted(),
             "YourNFToken: all tokens have been minted!"
